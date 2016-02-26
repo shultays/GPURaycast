3f12d43aa401d4e95bd1d119451a55a6143488ce
Engin Mercan <shultays@gmail.com>
2016-02-26 03:17:18 +0200

    added map texture

--- main.cpp
+++ main.cpp
@@ -3,25 +3,30 @@
 // Headers
 #include <GL/glew.h>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics/Image.hpp>
 
 // Shader sources
 const GLchar* vertexSource =
 "#version 150 core\n"
 "in vec2 position;"
-"in vec3 color;"
-"out vec3 Color;"
+"in vec2 vertexUV;"
+"out vec2 UV; "
 "void main()"
 "{"
-"    Color = color;"
+"    UV = vertexUV;"
 "    gl_Position = vec4(position, 0.0, 1.0);"
 "}";
+
 const GLchar* fragmentSource =
 "#version 150 core\n"
-"in vec3 Color;"
+"in vec2 UV;"
+"uniform sampler2D mapTexture;"
 "out vec4 outColor;"
+"uniform vec2 lookDir;"
+"uniform vec2 playerPos;"
 "void main()"
 "{"
-"    outColor = vec4(Color, 1.0);"
+"    outColor = texture(mapTexture, UV);"
 "}";
 
 int main() {
@@ -29,7 +34,7 @@ int main() {
 	settings.depthBits = 24;
 	settings.stencilBits = 8;
 
-	sf::Window window(sf::VideoMode(800, 600, 32), "OpenGL", sf::Style::Titlebar | sf::Style::Close, settings);
+	sf::Window window(sf::VideoMode(512, 512, 32), "OpenGL", sf::Style::Titlebar | sf::Style::Close, settings);
 
 	// Initialize GLEW
 	glewExperimental = GL_TRUE;
@@ -39,16 +44,16 @@ int main() {
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
-
+	glEnable(GL_TEXTURE_2D);
 	// Create a Vertex Buffer Object and copy the vertex data to it
 	GLuint vbo;
 	glGenBuffers(1, &vbo);
 
 	GLfloat vertices[] = {
-		-1.0f, 1.0f, 1.0f, 0.0f, 0.0f, // Top-left
-		1.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
-		1.0f, -1.0f, 0.0f, 0.0f, 1.0f, // Bottom-right
-		-1.0f, -1.0f, 1.0f, 1.0f, 1.0f  // Bottom-left
+		-1.0f, 1.0f, 0.0f, 1.0f, // Top-left
+		1.0f, 1.0f, 1.0f, 1.0f,  // Top-right
+		1.0f, -1.0f, 1.0f, 0.0f, // Bottom-right
+		-1.0f, -1.0f, 0.0f, 0.0f,  // Bottom-left
 	};
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
@@ -87,11 +92,40 @@ int main() {
 	// Specify the layout of the vertex data
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
 	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
 
-	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	GLint colAttrib = glGetAttribLocation(shaderProgram, "vertexUV");
 	glEnableVertexAttribArray(colAttrib);
-	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
+
+	sf::Image mapImage;
+	mapImage.create(20, 20, sf::Color());
+	if (!mapImage.loadFromFile("./map.bmp")) {
+		printf("no map data\n");
+		return 1;
+	}
+
+	GLuint mapTexture;
+	glGenTextures(1, &mapTexture);
+	glTexImage2D(
+		GL_TEXTURE_2D, 0, GL_RGBA,
+		mapImage.getSize().x, mapImage.getSize().y,
+		0,
+		GL_RGBA, GL_UNSIGNED_BYTE, mapImage.getPixelsPtr()
+		);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	GLint playerPosUnif = glGetUniformLocation(shaderProgram, "playerPos");
+	glUniform2f(playerPosUnif, 64.5f, 64.5f);
+
+	GLint lookDirUnif = glGetUniformLocation(shaderProgram, "lookDir");
+	glUniform2f(lookDirUnif, 1.0f, 0.0f);
+
 
 	bool running = true;
 	while (running) {
